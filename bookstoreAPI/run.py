76699680ef975dd4699df3b8daed84f12d81616b
Luis Fernando <lfb@alu.ufc.br>
2024-06-12 14:52:44 -0300

    Fixed 422 error and changed function names to snake case

--- bookstoreAPI/run.py
+++ bookstoreAPI/run.py
@@ -3,43 +3,26 @@ from models.user import User
 from models.author import Author
 
 app = FastAPI()
-
 @app.post("/user")
-async def postUser(user: User):
+async def post_user(user: User):
     return {"request body": user}
 
 @app.get("/user")
-async def getUserValidation(password:str):
+async def get_user_validation(password: str):
     return {"query parameter": password}
 
 @app.get("/book/{isbn}")
-async def getBookWithIsbn(isbn:str):
+async def get_book_with_isbn(isbn: str):
     return {"query changeable parameter": isbn}
 
 @app.get("/author/{id}/book")
-async def getAuthorsBook(id: int, category: str, order: str="asc"):
-    return{"query changeable parameter": f"{order} {category} {str(id)}"}
+async def get_authors_book(id: int, category: str, order: str = "asc"):
+    return {"query changeable parameter": f"{order} {category} {str(id)}"}
 
 @app.patch("/author/name")
-async def patchAuthorName(name: str = Body(..., embed=True)):
+async def patch_author_name(name: str = Body(..., embed=True)):
     return {"name in body": name}
 
-# gives 422 Unprocessable Entity with following:
-'''
-{
-    "user": {
-            "name": "user1",
-            "password": "pass1",
-            "email": "user@email.com",
-            "role": "admin"
-    },
-    "author": {
-        "name": "author1",
-        "book": ["book1","book2"]
-    },
-    "auth_token": "123456"
-}
-'''
 @app.post("/user/author")
-async def postUserAndAuthor(user:str, author: Author, auth_token: str = Body(..., embed=True)):
-    return {"user":user, "author":author, "auth_token": auth_token}
+async def post_user_and_author(user: User, author: Author, auth_token: str = Body(..., embed=True)):
+    return {"user": user, "author": author, "auth_token": auth_token}
